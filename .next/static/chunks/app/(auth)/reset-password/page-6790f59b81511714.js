e66b63b5ddb981b77eec4ef56f80cae8bc5735d3
Keyhan Mohammadi <105974026+mrcimiyagar@users.noreply.github.com>
2023-09-16 12:32:26 +0330

    fixed bug

---
+++ .next/static/chunks/app/(auth)/reset-password/page-6790f59b81511714.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[277,161,129],{1269:function(n,e,u){Promise.resolve().then(u.t.bind(u,8920,23))}},function(n){n.O(0,[920,667,488,744],function(){return n(n.s=1269)}),_N_E=n.O()}]);
